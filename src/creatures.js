f6fb0c51f2b26cf72c20aa14e0f98b28458c2766
huypham <9a8phamminhhuy@gmail.com>
2025-12-13 12:55:00 +0700

    Add creature data module

---
+++ src/creatures.js
@@ -0,0 +1,22 @@
+const JUNGLE_BETTLE = {
+  name: 'Jungle Bettle',
+  emoji: '<:MCJungleBettle:1448989040509452338>',
+  baseHealth: 15,
+  rarity: 'Common',
+  rarityEmoji: '★',
+  damage: { min: 1, max: 2 },
+  reward: { coins: { min: 50, max: 200 }, xp: { min: 10, max: 40 } },
+  levelDistribution: [
+    { level: 1, chance: 0.65 },
+    { level: 2, chance: 0.3 },
+    { level: 3, chance: 0.045 },
+    { level: 4, chance: 0.005 },
+  ],
+};
+
+const CREATURES = [JUNGLE_BETTLE];
+
+module.exports = {
+  CREATURES,
+  JUNGLE_BETTLE,
+};
