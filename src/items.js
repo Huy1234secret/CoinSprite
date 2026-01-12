b8cca2ec269a62cc4edde9c8a18d010d37a8a7fa
huypham <9a8phamminhhuy@gmail.com>
2026-01-12 11:42:13 +0700

    Update items.js

--- src/items.js
+++ src/items.js
@@ -43,7 +43,7 @@ const ITEMS = [
   {
     id: 'ITAdminPickaxe',
     name: 'Admin Pickaxe',
-    emoji: '<:ITAdminPickaxe:146012274332873082>',
+    emoji: '<:ITAdminPickaxe:1460122714328273082>',
     image: 'https://cdn.discordapp.com/emojis/146012274332873082.png?size=240&quality=lossless',
     rarity: 'Secret',
     type: 'Tool/Gear',
