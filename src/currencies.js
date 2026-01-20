9e32e61a754b3f6e38ee148fa4c630f4c17e0c5d
huypham <9a8phamminhhuy@gmail.com>
2026-01-20 22:34:57 +0700

    Add JAGMC currency and admin add command

---
+++ src/currencies.js
@@ -0,0 +1,35 @@
+const CURRENCIES = [
+  {
+    key: 'coins',
+    name: 'Coins',
+    emoji: '<:CRCoin:1447459216574124074>',
+  },
+  {
+    key: 'diamonds',
+    name: 'Diamonds',
+    emoji: '<:CRDiamond:1449260848705962005>',
+  },
+  {
+    key: 'prismatic',
+    name: 'Prismatic',
+    emoji: '<:CRPrismatic:1449260850945982606>',
+  },
+  {
+    key: 'jagmc',
+    name: 'JAGMC Token',
+    emoji: ':JAGMC:',
+    value: 499,
+    rarity: 'epic',
+    itemType: 'n/a',
+  },
+];
+
+const CURRENCIES_BY_KEY = CURRENCIES.reduce((acc, currency) => {
+  acc[currency.key] = currency;
+  return acc;
+}, {});
+
+module.exports = {
+  CURRENCIES,
+  CURRENCIES_BY_KEY,
+};
