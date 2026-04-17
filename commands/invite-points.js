3ae22a94887393165c14c7bdd8b667632d6c63c4
huypham <9a8phamminhhuy@gmail.com>
2026-04-17 13:20:05 +0700

    Add invite rewards, inventory commands, and PR admin controls

---
+++ commands/invite-points.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+const manager = require('../src/inviteRewardsManager');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('invite-points')
+    .setDescription('View your current Invite Points.'),
+
+  async init(client) {
+    await manager.init(client);
+  },
+
+  async execute(interaction) {
+    const userState = manager.loadGuildUserState(interaction.guildId, interaction.user.id);
+    if (userState.blacklisted) {
+      await interaction.reply({ ...manager.createBlacklistedPayload(), ephemeral: true });
+      return;
+    }
+
+    await interaction.reply({
+      ...manager.createInvitePointsPayload(interaction.user.username, userState.invitePoints),
+      ephemeral: true,
+    });
+  },
+
+  async handleGuildMemberAdd(member) {
+    await manager.onGuildMemberAdd(member);
+  },
+
+  async handleInviteCreate(invite) {
+    await manager.onInviteCreateOrDelete(invite);
+  },
+
+  async handleInviteDelete(invite) {
+    await manager.onInviteCreateOrDelete(invite);
+  },
+
+  async handleMessageCreate(message) {
+    await manager.onMessageCreate(message);
+  },
+};
