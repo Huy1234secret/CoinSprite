99005e2964a89adc346b2f1f9e0a020e92453ddc
huypham <9a8phamminhhuy@gmail.com>
2026-05-03 15:11:00 +0700

    Add PVP blackjack slash command

---
+++ commands/pvp-blackjack.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+const {
+  startPvpChallenge,
+  handlePvpBlackjackInteraction,
+  shouldLogBlackjackInteraction,
+} = require('../src/blackjackCore');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('pvp-blackjack')
+    .setDescription('Challenge another player to hidden-card PVP Blackjack')
+    .addUserOption((option) => option
+      .setName('user')
+      .setDescription('The player you want to challenge')
+      .setRequired(true))
+    .addStringOption((option) => option
+      .setName('amount')
+      .setDescription('PRcoin bet amount, like 1000, 5k, or all')
+      .setRequired(true)),
+  suppressCommandLog: true,
+
+  async execute(interaction) {
+    await startPvpChallenge(interaction);
+  },
+
+  shouldLogInteraction: shouldLogBlackjackInteraction,
+
+  async handleInteraction(interaction) {
+    return handlePvpBlackjackInteraction(interaction);
+  },
+};
