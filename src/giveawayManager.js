28146262fca03db6fa71901415930e41400431ec
huypham <9a8phamminhhuy@gmail.com>
2026-05-08 18:42:24 +0700

    Revert "Wire giveaway list command"
    This reverts commit 0c8e41b1854622c5a991055acb7dd9c2aee287a6.

--- src/giveawayManager.js
+++ src/giveawayManager.js
@@ -21,7 +21,6 @@ const {
 } = require('./giveawayUtils');
 const {
   buildHosterDmPayload,
-  buildGiveawayListPayload,
   buildLiveGiveawayPayload,
   buildRequirementButtonsPayload,
   buildRequirementModal,
@@ -157,15 +156,6 @@ async function handleRerollCommand(interaction, messageId) {
   }
 
   await interaction.reply({ content: 'Giveaway reroll forced.', flags: EPHEMERAL_FLAG });
-}
-
-async function handleListCommand(interaction) {
-  const state = runtime.getState();
-  const giveaways = Object.values(state.giveaways)
-    .filter((giveaway) => giveaway.messageId && giveaway.status !== 'completed')
-    .sort((left, right) => Number(left.endsAt || left.createdAt || 0) - Number(right.endsAt || right.createdAt || 0));
-
-  await interaction.reply(buildGiveawayListPayload(giveaways));
 }
 
 async function handleSetupMessageButton(interaction, draftId) {
@@ -501,7 +491,6 @@ module.exports = {
   handleDeleteCommand,
   handleInteraction,
   handleJoinButton,
-  handleListCommand,
   handleMessageCreate,
   handleMessageDelete,
   handleRerollCommand,
