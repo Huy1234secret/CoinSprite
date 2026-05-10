0714beb5a82a3187f84fe4b0e926fd673f3f7022
huypham <9a8phamminhhuy@gmail.com>
2026-05-10 15:33:59 +0700

    Remove adventure inventory and player level features

--- commands/balance.js
+++ commands/balance.js
@@ -1,16 +1,15 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const { getBalance, getSkillPoints } = require('../src/gamblingStore');
-const { COIN, SKILL_POINT, formatAbbreviated, formatNumber } = require('../src/gamblingConfig');
+const { getBalance } = require('../src/gamblingStore');
+const { COIN, formatAbbreviated } = require('../src/gamblingConfig');
 
 const COMPONENTS_V2_FLAG = MessageFlags.IsComponentsV2 ?? 32768;
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('balance').setDescription('Show your coin and skill point balance'),
+  data: new SlashCommandBuilder().setName('balance').setDescription('Show your coin balance'),
   suppressCommandLog: true,
 
   async execute(interaction) {
     const coins = getBalance(interaction.user.id);
-    const skillPoints = getSkillPoints(interaction.user.id);
 
     await interaction.reply({
       flags: COMPONENTS_V2_FLAG,
@@ -24,7 +23,6 @@ module.exports = {
               content: [
                 `### ${interaction.user.username}'s Balance`,
                 `* ${formatAbbreviated(coins)} ${COIN}`,
-                `* ${formatNumber(skillPoints)} ${SKILL_POINT}`,
               ].join('\n'),
             },
           ],
