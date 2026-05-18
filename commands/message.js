c19ca1b06396b5608738c24957b55489ca70b94a
huypham <9a8phamminhhuy@gmail.com>
2026-05-18 21:53:09 +0700

    Improve bot message replies (#612)

--- commands/message.js
+++ commands/message.js
@@ -7,7 +7,6 @@ const {
 const { logCommandSystem } = require('../src/commandLogger');
 
 const EPHEMERAL_FLAG = MessageFlags.Ephemeral ?? 64;
-const DISCORD_MESSAGE_ID_PATTERN = /^\d{16,20}$/;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -32,18 +31,11 @@ module.exports = {
         .setRequired(true)
         .setMaxLength(2000),
     )
-    .addStringOption((option) =>
-      option
-        .setName('replyto')
-        .setDescription('Optional message ID in that channel to reply to.')
-        .setRequired(false),
-    )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
     const targetChannel = interaction.options.getChannel('channel', true);
     const body = interaction.options.getString('message', true).trim();
-    const replyToMessageId = interaction.options.getString('replyto')?.trim() || null;
 
     if (!body) {
       await interaction.reply({ content: 'Message cannot be empty.', flags: EPHEMERAL_FLAG });
@@ -55,29 +47,11 @@ module.exports = {
       return;
     }
 
-    if (replyToMessageId && !DISCORD_MESSAGE_ID_PATTERN.test(replyToMessageId)) {
-      await interaction.reply({ content: '`replyto` must be a valid Discord message ID.', flags: EPHEMERAL_FLAG });
-      return;
-    }
-
     try {
-      let sentMessage;
-      if (replyToMessageId) {
-        const targetMessage = await targetChannel.messages.fetch(replyToMessageId).catch(() => null);
-        if (!targetMessage) {
-          await interaction.reply({
-            content: `Could not find message \`${replyToMessageId}\` in <#${targetChannel.id}> to reply to.`,
-            flags: EPHEMERAL_FLAG,
-          });
-          return;
-        }
-        sentMessage = await targetMessage.reply({ content: body });
-      } else {
-        sentMessage = await targetChannel.send({ content: body });
-      }
+      const sentMessage = await targetChannel.send({ content: body });
 
       await interaction.reply({
-        content: `Message sent to <#${targetChannel.id}> successfully${replyToMessageId ? ` as a reply to \`${replyToMessageId}\`` : ''}. Sent message ID: \`${sentMessage.id}\`.`,
+        content: `Message sent to <#${targetChannel.id}> successfully. Sent message ID: \`${sentMessage.id}\`.`,
         flags: EPHEMERAL_FLAG,
       });
     } catch (error) {
