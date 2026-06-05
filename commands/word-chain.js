fe29697cce9130b0bfe63403b3d3cf69ff56bfe1
Huy1234secret <174607649+Huy1234secret@users.noreply.github.com>
2026-06-05 23:01:08 +0700

    Use online dictionary for Word Chain

---
+++ commands/word-chain.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wordChainManager = require('../src/wordChainManager');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('word-chain')
+    .setDescription('Show the current Word Chain game status.'),
+  disableActionTimeout: true,
+
+  async init(client) {
+    await wordChainManager.init(client);
+  },
+
+  async execute(interaction) {
+    await wordChainManager.handleStatus(interaction);
+  },
+
+  async handleMessageCreate(message) {
+    await wordChainManager.handleMessageCreate(message);
+  },
+};
