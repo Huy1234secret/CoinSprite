65388f48c26485b8f3957f79dda44d94f38770ea
huypham <9a8phamminhhuy@gmail.com>
2026-06-16 11:04:43 +0700

    Fit message template thumbnails in interactions

--- commands/00-message-template-interactions.js
+++ commands/00-message-template-interactions.js
@@ -1,7 +1,18 @@
 const Module = require('module');
 const { handleMessageTemplateInteraction } = require('../src/messageTemplates');
+const { fitMessageThumbnailSquares } = require('../src/thumbnailFit');
 
 const previousLoad = Module._load;
+
+function wrapThumbnailReplies(interaction) {
+  if (!interaction || interaction.__coinSpriteThumbnailRepliesWrapped) return;
+  interaction.__coinSpriteThumbnailRepliesWrapped = true;
+  for (const method of ['reply', 'followUp']) {
+    if (typeof interaction[method] !== 'function') continue;
+    const nativeMethod = interaction[method].bind(interaction);
+    interaction[method] = async (payload, ...args) => nativeMethod(await fitMessageThumbnailSquares(payload), ...args);
+  }
+}
 
 Module._load = function registerMessageTemplateInteractions(request, parent, isMain) {
   const exported = previousLoad.call(this, request, parent, isMain);
@@ -12,6 +23,7 @@ Module._load = function registerMessageTemplateInteractions(request, parent, isM
     if (!client.__messageTemplateInteractionHandler) {
       client.__messageTemplateInteractionHandler = true;
       client.on('interactionCreate', (interaction) => {
+        wrapThumbnailReplies(interaction);
         handleMessageTemplateInteraction(interaction).catch((error) => {
           console.error('Message template component interaction failed:', error);
         });
