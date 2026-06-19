b15c325eb7eb4d979c347e15bdbd1f0b98e60a5f
huypham <9a8phamminhhuy@gmail.com>
2026-06-19 17:21:36 +0700

    Load admin images directly from repository

--- admin/user-data.js
+++ admin/user-data.js
@@ -15,7 +15,7 @@
       image.setAttribute('aria-hidden', 'true');
       button.prepend(image);
     }
-    if (image.getAttribute('src') !== '/CoinSprite/images/data.png') image.src = '/CoinSprite/images/data.png';
+    if (image.getAttribute('src') !== 'https://raw.githubusercontent.com/Huy1234secret/CoinSprite/main/images/data.png') image.src = 'https://raw.githubusercontent.com/Huy1234secret/CoinSprite/main/images/data.png';
   }
 
   function ensureTicketBlacklistField() {
