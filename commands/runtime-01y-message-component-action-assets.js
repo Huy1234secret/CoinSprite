a7e43f88224fa8cf1141ebe89168c894014c5426
huypham <9a8phamminhhuy@gmail.com>
2026-07-06 13:45:32 +0700

    Consolidate 01y-message-component-action-assets.js

---
+++ commands/runtime-01y-message-component-action-assets.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const INDEX_PATH = path.resolve(__dirname, '..', 'admin', 'index.html');
+const nativeReadFileSync = fs.readFileSync.bind(fs);
+
+if (!fs.__coinSpriteMessageComponentActionAsset) {
+  fs.__coinSpriteMessageComponentActionAsset = true;
+  fs.readFileSync = function readFileWithMessageComponentActions(filePath, ...args) {
+    const value = nativeReadFileSync(filePath, ...args);
+    if (path.resolve(String(filePath)) !== INDEX_PATH || typeof value !== 'string') return value;
+    if (value.includes('/admin/message-component-actions.js')) return value;
+    return value
+      .replace(
+        '</head>',
+        '  <link rel="stylesheet" href="/admin/message-component-actions.css?v=action-save-3">\n</head>',
+      )
+      .replace(
+        '</body>',
+        [
+          '  <script src="/admin/message-component-actions.js?v=action-save-3" defer></script>',
+          '</body>',
+        ].join('\n'),
+      );
+  };
+}
+
+module.exports = {};
