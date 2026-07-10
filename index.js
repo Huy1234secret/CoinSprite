8c2563352dade14413d3f3e551178e0cc6683938
huypham <174607649+Huy1234secret@users.noreply.github.com>
2026-07-10 12:31:29 +0700

    Remove GAG2 stock poster

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const { loadState, saveState } = require('./src/ticketSystemStore');
 const inviteRewardsManager = require('./src/inviteRewardsManager');
 const { deleteGuildConfig, ensureGuildConfig, getEnabledGuildIds, getGuildConfig, isGuildEnabled } = require('./src/serverConfig');
 const { registerConsolidatedAdminCommands, startAdminServer } = require('./src/adminServer');
-const { startGag2StockPoster } = require('./src/gag2Stock/manager');
 const EPHEMERAL_FLAG = MessageFlags.Ephemeral ?? 64;
 const COMPONENTS_V2_FLAG = MessageFlags.IsComponentsV2 ?? 32768;
 const TICKET_ACTION_SELECT_PREFIX = 'ticket:actions:';
@@ -552,7 +551,6 @@ client.once(Events.ClientReady, async () => {
   startAdminServer(client);
   await initCommandModules();
   await runInviteRewardHook('init', client); // FIXED: avoids calling .catch() on undefined legacy hook output.
-  await startGag2StockPoster(client);
   await registerSlashCommands();
 });
 
