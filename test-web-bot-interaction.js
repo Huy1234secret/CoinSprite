19b4b4f9cb0ba2dae624f4fbbbbdcf2eca7119cf
huypham <9a8phamminhhuy@gmail.com>
2026-08-08 21:47:27 +0700

    Revert "fix: support remote HTTP fetch fallback for level card media (#976)" (#977)
    This reverts commit 004b70b13a00682c4c956e8d05906be3e65eaa16.

--- test-web-bot-interaction.js
+++
@@ -1,6 +0,0 @@
-const { renderPublishedLevelCard, saveLevelCardDesign, renderLevelCard } = require('./src/leveling');
-
-async function test() {
-  console.log("Mocking scenario where fetch external succeeds but returns an image that lacks something?");
-}
-test();
